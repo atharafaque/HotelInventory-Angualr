ceaa99c3fee6090c7ebc55dcb76eb215ece6227e
Athar Afaque <80690156+atharafaque@users.noreply.github.com>
2024-03-25 15:23:14 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  // template: `<h1>Hello World from inline template</h1> 
+  // <p> Angular is Awesome </p>`,
+  styleUrls: ['./app.component.scss'],
+
+})
+export class AppComponent {
+  title = 'hotelinventoryapp';
+}
